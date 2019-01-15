167c62e2d0cf47c174d133221d92a8c6814a5548
AnthonyClausing <AnthonyClausing@gmail.com>
2019-01-15 18:53:11 -0500

    added ability to add a event to a dummy event list on GraphiQL

--- app.js
+++ app.js
@@ -5,16 +5,37 @@ const { buildSchema } = require('graphql');
 
 const app = express();
 
+const events = [];
+
 app.use(bodyParser.json());
+// Good thing about graphql is that you can tell the backend exactly what you want in your query from the frontend
+// Adding ! after a type makes it not optional (not null)
+//better practice to create a type for a list of args in createEvent instead of having a long list written out in the parentheses
 
 app.use('/graphql', graphqlHttp({
   schema: buildSchema(`
+    type Event {
+      _id: ID!
+      title: String!
+      description: String!
+      price: Float!
+      date: String! 
+    }
+
+    input EventInput {
+      title: String!
+      description: String!
+      price: Float!
+      date: String!
+
+    }
+
     type RootQuery {
-      events: [String!]!
+      events: [Event!]!
     }
 
     type RootMutation {
-      createEvent(name: String): String
+      createEvent(eventInput: EventInput): Event
     }
 
     schema {
@@ -24,11 +45,18 @@ app.use('/graphql', graphqlHttp({
   `),
   rootValue: {
     events: () => {
-      return ['Romantic Cooking', 'Sailing', 'All-Night Coding']
+      return events;
     },
     createEvent: (args) => {
-      const eventName = args.name;
-      return eventName;
+      const event = {
+        _id: Math.random().toString(),
+        title: args.eventInput.title,
+        description: args.eventInput.description,
+        price: +args.eventInput.price,
+        date: args.eventInput.date
+      }
+      events.push(event);
+      return event
     }
   },
   graphiql: true
