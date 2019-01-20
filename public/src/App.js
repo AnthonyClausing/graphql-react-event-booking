d53e57274b8c94ef41062c3eeaf279160446bbf9
AnthonyClausing <anthonyclausing@gmail.com>
2019-01-20 17:13:02 -0500

    navbar createdstyled

--- public/src/App.js
+++ public/src/App.js
@@ -1,21 +1,28 @@
 import React, { Component } from 'react';
-import './App.css';
 import { BrowserRouter, Route, Redirect, Switch } from 'react-router-dom';
 
 import AuthPage from './pages/Auth';
 import EventsPage from './pages/Events';
 import BookingsPage from './pages/Bookings';
+import MainNavigation from './components/Navigation/MainNavigation';
+
+import './App.css';
 
 class App extends Component {
   render() {
     return (
       <BrowserRouter>
-        <Switch>
-          <Redirect from="/" to="/auth" exact/>
-          <Route path="/auth" component={AuthPage}/>
-          <Route path="/events" component={EventsPage}/>
-          <Route path="/bookings" component={BookingsPage}/>
-        </Switch>
+        <div id="whole-page">
+          <MainNavigation/>
+          <main className="main-content">
+            <Switch>
+              <Redirect from="/" to="/auth" exact/>
+              <Route path="/auth" component={AuthPage}/>
+              <Route path="/events" component={EventsPage}/>
+              <Route path="/bookings" component={BookingsPage}/>
+            </Switch>
+          </main>
+        </div>
       </BrowserRouter>
     );
   }
