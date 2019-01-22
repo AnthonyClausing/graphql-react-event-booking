5e9ef4de06f2fb7d928cbf7feb579c71cfca54c0
AnthonyClausing <anthonyclausing@gmail.com>
2019-01-22 16:53:52 -0500

    modal created can be opened and closed

--- public/src/pages/Events.js
+++ public/src/pages/Events.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react';
 
+import Modal from '../components/Modal/Modal';
+import Backdrop from '../components/Backdrop/Backdrop';
+import './Events.css';
+
 class EventsPage extends Component{
+  state = {
+    creating: false
+  }
+
+  startCreateEventHandler = () => {
+    this.setState({creating: true});
+  }
+  
+  modalCancel = () => {
+    this.setState({creating: false});
+  };
+
+  modalConfirm = () =>{
+    this.setState({creating: false});
+  };
 
   render(){
-    return (<h1>The Events Page</h1>);
+    return (
+      <React.Fragment>
+        {this.state.creating && <Backdrop/>}
+        {this.state.creating && <Modal title="Add event" canCancel canConfirm onCancel={this.modalCancel} onConfirm={this.modalConfirm}>
+        <p>modal content</p>
+        </Modal>}
+        <div className="events-control">
+          <p>Share your own events!</p>
+          <button className="btn" onClick={this.startCreateEventHandler}>Create Event</button>
+        </div>
+      </React.Fragment>
+    );
   }
 }
 
