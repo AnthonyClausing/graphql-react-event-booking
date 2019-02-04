1d3026d69eeaf1536a449853da755f949637ad39
AnthonyClausing <anthonyclausing@gmail.com>
2019-02-04 17:09:36 -0500

    added dataloader to reduce backend roundtrips

--- graphql/resolvers/merge.js
+++ graphql/resolvers/merge.js
@@ -1,7 +1,17 @@
+const DataLoader = require('dataloader');
+
 const Event = require('../../models/event');
 const User = require('../../models/user');
 
 const { dateToString } = require('../../helpers/date');
+
+const eventLoader = new DataLoader((eventIds) => {
+  return events(eventIds);
+});
+
+const userLoader = new DataLoader((userIds) => {
+  return User.find({_id: {$in: userIds}})
+});
 
 //Using both these functions allows for the obtaining of any related data for as deep as necessary in a graphql query
 //This function gets all events in a list of event ids using the mongodb operator $in
@@ -16,8 +26,8 @@ const events = async eventIds => {
 
 const singleEvent = async eventId => {
   try{
-    const event = await Event.findById(eventId);
-    return transformEvent(event);
+    const event = await eventLoader.load(eventId.toString());
+    return event;
   } catch(err) {
     throw err;
   }
@@ -46,11 +56,11 @@ const transformBooking = booking =>{
 
 const user = async userId => {
   try{ 
-    const user = await User.findById(userId)
+    const user = await userLoader.load(userId.toString());
     return {
       ...user._doc, 
       _id: user.id, 
-      createdEvents: events.bind(this, user._doc.createdEvents)
+      createdEvents: () => eventLoader.loadMany.bind(this, user._doc.createdEvents)
     };
   } catch(err) {
     throw err;
